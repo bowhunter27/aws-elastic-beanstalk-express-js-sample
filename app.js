d6ed348eb7dcfe9552fa5c172a7eea239ac700bc
Chad Cornelius <chadcornelius@Chads-Mac-mini.local>
2024-09-16 19:04:52 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Chad!'));
+app.get('/', (req, res) => res.send('Let's Do this Chad!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
